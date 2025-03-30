9eaf5d1161d6d07a93748d89b0f0b8a493bf3a0f
1spyral <83098914+1spyral@users.noreply.github.com>
2025-03-30 16:23:38 -0400

    Set up split-screen layout

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import SideBar from "@/components/SideBar.vue"
+import Editor from "@/components/Editor.vue"
+import PDFRenderer from "@/components/PDFRenderer.vue"
 </script>
 
 <template>
   <div class="main">
-    <SideBar/>
-
+    <SideBar />
+    <Editor />
+    <PDFRenderer />
   </div>
 </template>
 
 <style scoped>
 .main {
-  background: var(--offwhite);
+  background: var(--paper-yellow);
   display: flex;
   flex-direction: row;
   justify-content: space-between;
---
+++ frontend/src/components/PDFRenderer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="pdf-renderer">PDF</div>
+</template>
+
+<style scoped>
+.pdf-renderer {
+  flex: 1;
+  background: var(--dark-background);
+}
+</style>
