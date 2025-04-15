becce7da8c0aaedf78acb8e66f1243884f696de6
1spyral <83098914+1spyral@users.noreply.github.com>
2025-04-14 21:05:52 -0400

    Set up new backend

---
+++ backend/build.js
@@ -0,0 +1,22 @@
+import { build } from "esbuild"
+
+await build({
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    platform: "node",
+    outfile: "./dist/index.js",
+    target: ["node22"],
+    external: [
+        "path",
+        "express",
+        "fs",
+        "fs/promises",
+        "http",
+        "https",
+        "stream",
+        "stream/promises",
+        "zlib",
+        "url",
+    ],
+    format: "cjs",
+})
